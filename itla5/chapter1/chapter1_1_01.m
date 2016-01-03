f0b6eec83758491e16a497ee3177fed47631d1f7
sunyongjie <sunyongjie1984@gmail.com>
2016-01-03 14:52:29 +0800

    modify

--- itla5/chapter1/chapter1_1_01.m
+++ itla5/chapter1/chapter1_1_01.m
@@ -1,6 +1,14 @@
+clc;
 v = [ 1 ; 1 ]
 w = [ 2 ; 3 ]
-3 * v + 5 * w
+kk = 3 * v + 5 * w
+quiver( 0, 0, kk( 1 ), kk( 2), 1 );
+
+kk_x = sprintf('%f', kk( 1 ) );
+kk_y = sprintf('%f', kk( 2 ) );
+test_kk = sprintf('%s%s%s%s%s', '3v + 5w', '  ( ', kk_x, ', ', kk_y, ' )');
+text( kk( 1 ) / 2, kk( 2 ) / 2, test_kk ); 
+hold on
 
 w = [ -1 ; 2 ]
 v = [ 4 ; 2 ]
@@ -10,39 +18,40 @@ v + w
 quiver( 0, 0, -1, 2, 1)
 text( -0.5, 1, '  w = ( -1, 2 ) ' );
 
-hold
+hold on
 quiver( 0, 0, 4, 2, 1 )
 text( 2, 1, '  v = ( 4, 2 ) ');
-
+hold on
 quiver( 0, 0, 3, 4, 1 )
 text( 1.5, 2, '  ( v + w ) = ( 3, 4 )');
 % text( 1.5, 2, '\fontsize{18} ( v + w ) = ( 3, 4 )');
-
+hold on
 quiver( 0, 0, 5, 0, 1 )
 text( 2.5, 0.1, '  ( v + ( -w ) ) = ( 5, 0 )');
 
-
+hold on
 quiver( 4, 2, -1, 2, 1, '--' )
 text( 3.5, 3, '  w ');
 
-
+hold on
 quiver( -1, 2, 4, 2, 1, '--' )
 text( 1, 3, '  v ');
-
+hold on
 quiver( 0, 0, 1, -2, 1, '--' )
 text( 0.5, -1, '  -w ');
-
+hold on 
 quiver( 4, 2, 1, -2, 1, '--' )
 text( 4.5, 1, '  -w ');
-
+hold on
 quiver( 1, -2, 4, 2, 1, '--' )
 text( 3, -1, '  v ');
 
 title({'Figure 1.1: Vector addition v + w = ( 3, 4 ) produces the diagonal of a parallelogram.';...
     'The reverse of w is -w. The linear combination on the right is v - w = ( 5, 0 ).'},'FontSize',12);
 xlabel('x'); ylabel('y');
-legend(' w ', ' v ', ' v + w ', ' v + ( -w ) ', ' w ', ' v ', ' -w ', ' -w ', ' v ' )  
-     
+legend('kk', ' w ', ' v ', ' v + w ', ' v + ( -w ) ', ' w ', ' v ', ' -w ', ' -w ', ' v ' )  
+%legend(' w ', ' v ', ' v + w ', ' v + ( -w ) ', ' w ', ' v ', ' -w ', ' -w ', ' v ' )  
+  
 
 % t=[0,2,4,6,8];
 % y1=[0,0.075,0.135,0.1875,0.255];
