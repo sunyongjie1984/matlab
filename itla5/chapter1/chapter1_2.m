dd5bf3fb0953cbed6af3c092b3f8daac1bb2ef6f
sunyongjie <sunyongjie1984@gmail.com>
2018-06-18 07:45:50 +0800

    modify

--- itla5/chapter1/chapter1_2.m
+++ itla5/chapter1/chapter1_2.m
@@ -1,3 +1,15 @@
 w = [ -1 ; 2 ] % define column vector w
 v = [ 4  ; 2 ] % define column vector v
 k = dot( w , v ) %  dot product
+
+quiver( 0, 0, w( 1 ), w( 2 ), 1)
+text( -0.5, 1, '  w = ( -1, 2 ) ' )
+
+hold
+quiver( 0, 0, v( 1 ), v( 2 ), 1)
+text( 2, 1, '  v = ( 4, 2 ) ');
+
+
+title('dot product','FontSize',12);
+xlabel('x'); ylabel('y');
+legend(' w ', ' v ')  
