a964c632f2894a0bbaae025ca7155882944a0130
sunyongjie <sunyongjie1984@gmail.com>
2018-06-17 14:15:26 +0800

    modify

--- itla5/chapter1/chapter1_1_02.m
+++ itla5/chapter1/chapter1_1_02.m
@@ -1,22 +1,22 @@
 v = [ 1; 1; -1; ]
 w = [ 2; 3; 4; ]
-v + w 
+k = v + w 
 
 
-quiver3( 0, 0, 0, 1, 1, -1, 1 )
+quiver3( 0, 0, 0, v( 1 ), v( 2 ), v( 3 ), 1 )
 text( 1, 1, -1, '  v = ( 1, 1, -1 ) ' );
 
 hold
 
-quiver3( 0, 0, 0, 2, 3, 4, 1 )
+quiver3( 0, 0, 0, w( 1 ), w( 2 ), w( 3 ), 1 )
 text( 2, 3, 4, '  w = ( 2, 3, 4 ) ' );
 
-quiver3( 0, 0, 0, 3, 4, 3, 1 )
-text( 3, 4, 3, '  w = ( 3, 4, 3 ) ' );
+quiver3( 0, 0, 0, k( 1 ), k( 2 ), k( 3 ), 1 )
+text( 3, 4, 3, '  k = ( 3, 4, 3 ) ' );
 
 
 title('vectors in three dimensions','FontSize',12);
 xlabel('x');
 ylabel('y');
 zlabel('z');
-legend(' v ', ' w ', ' v + w ' )  
+legend(' v ', ' w ', ' k = ( v + w ) ' )  
