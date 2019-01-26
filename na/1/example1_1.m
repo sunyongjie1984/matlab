9b25c4de1faac081639f3397daa881cb5c9272ff
yongjies <yongjies@cadence.com>
2019-01-26 14:43:44 +0800

    modify

--- na/1/example1_1.m
+++ na/1/example1_1.m
@@ -1,7 +1,12 @@
-f = @( x ) x ^ 3 + x -1
+f = @( x ) x ^ 3 + x -1 % define a function
 startPos = 0.0
 endPos = 1.0
-t = 0.00005
+t = 0.001
 
+root = bisect( f, startPos, endPos, t )
+input('press any key to continue');
+
+
+t = 0.00005 % different precision
 root = bisect( f, startPos, endPos, t )
 
