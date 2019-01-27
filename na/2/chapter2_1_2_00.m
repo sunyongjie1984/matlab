0609d83cba1b570b24daaaa5804aeee74812e1d5
yongjies <yongjies@cadence.com>
2019-01-27 11:31:03 +0800

    modify

--- na/2/chapter2_1_2_00.m
+++ na/2/chapter2_1_2_00.m
@@ -1,5 +1,12 @@
-A = [ 1 2 -1; 2 1 -2; -3 1 1; ]
-b = [ 3; 3; -6 ]
+format long
+% A = [ 1 2 -1; 2 1 -2; -3 1 1; ]
+% b = [ 3; 3; -6 ]
+
+A = rand( 3, 3 )
+b = rand( 3, 1 )
+
+% A = [ 1 2 2; 4 8 9; 0 3 2; ]
+% b = [ 1; 3; 1; ]
 [ A b ] % augmented matrix
 
 [ U, c ] = gaussEYongjies( A, b )
@@ -7,3 +14,10 @@ b = [ 3; 3; -6 ]
 [ U c ] % new augmented matrix
 
 x = gaussBSYongjies( U, c )
+
+x_from_library = A\b
+
+if ( x ~= x_from_library )
+    error( 'my gauss errors in this case' )
+end
+
--- na/2/gaussBSYongjies.m
+++ na/2/gaussBSYongjies.m
@@ -4,10 +4,15 @@ function x = gaussBSYongjies( U, c )
 [ rowSize, ~ ] = size( U );
 n = rowSize;
 x = c;
-
+% when i == 3, solve x( 3 ):
+% x( 3 ) = c( 3 ) / U( 3, 3 )
+% when i == 2, slove x( 2 ):
+% x( 2 ) = ( c( 2 ) - U( 2, 3 ) * x( 3 ) ) / U( 2, 2 )
+% when i == 1, slove x( 1 ):
+% x( 1 ) = ( c( 1 ) - U( 1, 2 ) * x( 2 ) ) - U( 1, 3 ) * x( 3 ) ) / U(1, 1)
 for i = n : -1 : 1
     for j = i + 1 : 1 : n
-        U( i ) = U( i ) - U( i, j ) * x( j );
+         c( i ) = c( i ) - U( i, j ) * x( j );
     end
     x( i ) = c( i ) / U( i, i );
 end
