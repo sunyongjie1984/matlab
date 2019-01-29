778ee9b72c988c6a68f140553fd78ae99c77daec
yongjies <yongjies@cadence.com>
2019-01-29 12:49:52 +0800

    modify

--- na/1/fpi.m
+++ na/1/fpi.m
@@ -3,9 +3,12 @@
 %Input: inline function g, starting guess x0, 
 %       number of steps k
 %Output: Approximate solution xc
+
 function xc = fpi( g, x0, k )
+
 x( 1 ) = x0;
 for i = 1 : k
   x( i + 1 ) =g( x( i ) );
 end
+
 xc = x( k + 1 );
