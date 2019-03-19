d81196bac8364c299c13daa48cddc19775383bfe
yongjies <yongjies@cadence.com>
2019-03-19 10:43:52 +0800

    modify

--- na/1/nrmYongjies02.m
+++ na/1/nrmYongjies02.m
@@ -13,64 +13,46 @@ syms x
 g(x) = diff( f( x ), x );
 
 x = zeros( k + 1, 1 );
-x( 1 ) = x0;
-str = sprintf( '%.15f  %d', x( 1 ), 0);
-text( 0.1, 0.9, str );
-plot( x( 1 ), 0, '*' ), pause( 1 ), hold on
-str = sprintf( '%.4f', x( 1 ) );
-text( x( 1 ), 0.0001, str )
-plot( x( 1 ), f( x( 1 ) ), '*' ), pause( 1 ), hold on
-str = sprintf( '%.4f %.4f', x( 1 ), eval( f( x( 1 ) ) ) );
-text( x( 1 ), eval( f( x( 1 ) ) ), str )
 
-m = g( x( 1 ) )
 syms z q( z )
-q( z ) = f( x( 1 ) ) + m * ( z - x( 1 ) )
-
-fplot( q, [ x( 1 ), 0.15 ], 'm--' ), hold on, pause( 1 )
 
 for i = 1 : k
-    x( i + 1 ) = x( i ) - f( x( i ) ) / g( x( i ) )
-    str = sprintf('%.15f  %d', x( i + 1 ), i)
-    text( 0.1, 0.9 - 0.05 .* i, str );
-    plot( x( i + 1 ), 0, '*' ), pause( 1 )
-    if i < 5
-        str = sprintf( '%.4f', x( i + 1 ) );
-        text( x( i + 1 ), 0.0001, str )
+    if 1 == i
+        x( i ) = x0;
+    else
+        x( i ) = x( i - 1 ) - f( x( i - 1 ) ) / g( x( i - 1 ) )
     end
-    plot( x( i + 1 ), f( x( i + 1 ) ), '*' ), pause( 1 )
-    if i < 5
-        str = sprintf( '%.4f %.4f', x( i + 1 ), eval( f( x( i + 1 ) ) ) );
-        text( x( i + 1 ), eval( f( x( i + 1 ) ) ), str )
+    str = sprintf('%.15f  %d', x( i ), i)
+    text( 0.1, 0.9 - 0.05 * i, str ), pause( 2 )
+    plot( x( i ), 0, '*' ), pause( 2 )
+    if i < 50
+        str = sprintf( '%.7f %d', x( i ), i );
+        text( x( i ), 0.000, str ), pause( 2 )
+    end
+    plot( [x( i), x(i)], [0, f(x(i))], 'r'); hold on, pause( 2 )
+    plot( x( i ), f( x( i ) ), '*' ), pause( 2 )
+    if i < 50
+        str = sprintf( '%.4f %.15f %d', x( i ), eval( f( x( i ) ) ), i );
+        text( x( i ), eval( f( x( i ) ) ), str ), pause( 2 )
     end
     set( gcf, 'color', 'w' ) % set back ground white
-    m = g( x( i + 1 ) );
-    q( z ) = f( x( i + 1 ) ) + m * ( z - x( i + 1 ) )
-    %root = fzero( q, 0 )
-    root = solve( q, z, 'Real', true )
+    m = g( x( i ) );
+    q( z ) = f( x( i ) ) + m * ( z - x( i ) )
+    root = solve( q, z, 'Real', true ) % try to use function solve
+    % the root can be calculated by the follwing, but now I just want to find the end of the tangent line
+    %x( i ) = x( i - 1 ) - f( x( i - 1 ) ) / g( x( i - 1 ) )
     root = eval(root)
-
-    if root > x( i + 1 )
-        a = x( i + 1);
+    if root > x( i )
+        a = x( i );
         b = root;
     else
         a = root;
-        b = x( i + 1);
+        b = x( i );
     end
-    a = a - 0.02;
-    b = b + 0.02;
 
-    if 1 == i
-        fplot( q, [ a , b ], 'm--' ), hold on, pause( 1 );
-    elseif 2 == i
-        fplot( q, [ a , b ], 'm--' ), hold on, pause( 1 );
-    elseif 3 == i
-        fplot( q, [ a , b ], 'm--' ), hold on, pause( 1 );
-    elseif 4 == i
-        fplot( q, [ a , b ], 'm--' ), hold on, pause( 1 );
-    else
-%         fplot( f, [ -0.8, 1 ], '-' )
-%         hold on  grid
+    if i < 100 % draw all the tangent lines
+        fplot( q, [ a , b ], 'm--' ), hold on, pause( 2 );
+
     end
 end
         fplot( f, [ -0.8, 1 ], '-' )
