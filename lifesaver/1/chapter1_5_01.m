31e134851cd65dae349bb1f1ff3fd465b61f011f
yongjies <yongjies@cadence.com>
2019-04-23 18:08:21 +0800

    modify

--- lifesaver/1/chapter1_5_01.m
+++ lifesaver/1/chapter1_5_01.m
@@ -3,32 +3,26 @@ x = [ -10: 0.5: 10 ];
 
 m = 1;
 b = 0.5;
-y = m * x + b;
+y = 0 * x
+plot( x, y, '-bo' )
 hold on;
+
+y = [ -20: 5: 25 ];
+x = 0 * y
+plot( x, y, '-ro' )
+hold on;
+
+x = [ -10: 0.5: 10 ];
+
+m = 1;
+b = 0.5;
+y = m * x + b;
 plot( x, y, '-*' )
+hold on;
+
 m = 2
 y = m * x + b;
-
 plot( x, y, '-*' )
-set(gca, 'XColor', 'red');
-grid;
 hold on;
-set(gca, 'YColor', 'Blue');
-grid;
-hold on;
-
-ylim = get(gca, 'Ylim')
-plot( [ 0, 0 ], ylim, '-' );
-hold on;
-ylim = get(gca, 'Ylim')
-plot( [ -0.5, -0.5 ], ylim, '--');
-hold on;
-xlim = get(gca, 'Xlim')
-plot( xlim, [ 0, 0 ], '-' );
-hold on;
-xlim = get(gca, 'Xlim')
-plot( xlim, [ -0.5, -0.5 ], '--' );
-hold on;
-
 
 legend(' y = 1 * x + 0.5 ', ' y = 2 * x + 0.5 ', ' x = 0 ', ' x = -0.5', ' y = 0 ', ' y = -0.5' )
